9510d7d1f5c3c51e1dc9f21a148d7654768a569e
sethanderson8 <32688614+sethanderson8@users.noreply.github.com>
2026-09-25 12:28:52 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # go reverse, two pointers, past, future
+        # any time the past is lower than futuer, "sell",
+        # add to max maxProfit
+        # anytime the past is greater than future, future = past, past -=1
+        # keep searching
+        # repeat til end
+
+        future = len(prices) - 1
+        past = len(prices) - 2
+        maxProf = 0
+
+        while past >= 0:
+            if prices[past] < prices[future]:
+                # sell
+                maxProf += (prices[future] - prices[past])
+                # future is now past
+                future = past
+            else:
+                # this is when prices are either equal or past is higher than future
+                # in this case, 
+                future = past
+            past -= 1
+
+        return maxProf
